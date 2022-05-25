c58b7434f7ff74f46f9ad2d629e547de978474ed
Paloma <casagrande.paloma@gmail.com>
2022-05-25 10:57:00 -0300

    feat tela login logica

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {useState} from 'react';
+import axios from "axios";
+import logo from '../assets/logo.png'
 
 
 function LoginPage(){
+    const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login'
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [load, setLoad] = useState(false);
+    let navigate = useNavigate();
+
+    function login(e){
+        e.preventDefault();
+        setLoad(true)
+        
+        const formData = {
+            email,
+            password
+        }
+
+        console.log('uhull', formData);
+
+
+        const promise = axios.post(URL, formData);
+
+        promise.then(handleSuccess);
+        promise.catch(handleError);
+    }
+
+    function handleSuccess(response){
+        console.log(response.data);
+        navigate('/hoje')
+    };
+
+    function handleError(err){
+        alert (err.response.data.message);
+        setLoad(false);
+    }
+
+
     return(
+        
         <>
-            <h1> Essa é uma LoginPage</h1>
+            <img src={logo} alt='Logo' />
+            <form onSubmit={login}>
+                <input disabled={load} type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value) } required/>
+                <input disabled={load} type='password' placeholder='senha' value={password} onChange={(e) => setPassword(e.target.value) } required />
+                <button type='submit' disabled={load} > Entrar </button>
+            </form>
+
             <Link to='/cadastro'>
-                <button> ir pra cadastro </button>
+                <span> Não tem uma conta? Cadastre-se! </span>
             </Link>
+        
         </>
     )
 }
