e01996d2d8341b0836304adfe72e5881b7611615
Paloma <casagrande.paloma@gmail.com>
2022-05-29 22:04:33 -0300

    feat tela hoje - layout

--- src/components/TodayPage.js
+++ src/components/TodayPage.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import * as dayjs from 'dayjs'
 import 'dayjs/locale/pt-br' // import locale
 
@@ -69,7 +70,7 @@ function TodayPage(){
             return <p> Nenhum hábito concluído ainda</p>
         } else{
             setPercentage((habitsDone/habitsList.length)*100)
-            return <p> {habitsList.length} {qtd} {percentage} </p>
+            return <Subtitle> {` ${percentage}% dos hábitos concluídos`} </Subtitle>
         }
 
     }
@@ -86,15 +87,16 @@ function TodayPage(){
     
     return(
         <>
-        <Container>
+        <LocalStyle/>
         <Header/>
-        <MyTodayHeader>
-            <h1>{Today}</h1>
-            <h2>{todayProgress}</h2>
-        </MyTodayHeader>
-        <MyTodayHabits>
-            {renderMyTodayHabits()}
-        </MyTodayHabits>
+        <Container>
+            <MyTodayHeader>
+                <h1>{Today}</h1>
+                <p>{todayProgress}</p>
+            </MyTodayHeader>
+            <MyTodayHabits>
+                {renderMyTodayHabits()}
+            </MyTodayHabits>
         </Container>
         <Menu/>
         </>
@@ -198,8 +200,12 @@ function Habit({id, name, isDone, currentSequence, highestSequence, setHabitsLis
         <MyHabitBox checked={checked} isRecord={isRecord}>
             <div>
                 <h1>{name}</h1>
-                <p>{`Sequência atual: `}</p> <h5>{`${currentSequence} dias`}</h5>
-                <p> {`Seu recorde: `}</p> <h6>{`${highestSequence} dias`}</h6>
+                <Text>
+                    <p>{`Sequência atual: `}</p> <h5>{`  ${currentSequence} dias`}</h5>
+                </Text>
+                <Text>
+                    <p> {`Seu recorde: `}</p> <h6>{`  ${highestSequence} dias`}</h6>
+                </Text>
             </div>
             <button onClick={checkHabit}><ion-icon name="checkmark"></ion-icon></button>
         </MyHabitBox>
@@ -208,48 +214,112 @@ function Habit({id, name, isDone, currentSequence, highestSequence, setHabitsLis
 
 export default TodayPage;
 
+const LocalStyle = createGlobalStyle`
+body {
+    background-color: #F2F2F2;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+}
+`
+
 const Container = styled.div`
-margin: 70px auto 110px auto;
+    width: 100%;
+    margin: 70px auto 75px auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
 `
 
 const MyTodayHeader = styled.div`
-    h1{    
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 28px 17px 0 17px;
+
+    h1{
         font-size: 23px;
         line-height: 29px;
+        padding: 0 17px 2px 17px;
 
         color: #126BA5;
     }
+
+    p{
+        font-size: 18px;
+        line-height: 22px;
+        padding-left: 9px;
+
+        color: #BABABA;
+    }
+
 `
-const MyTodayHabits = styled.div``
+
+const Subtitle = styled.div`
+    font-size: 18px;
+    line-height: 22px;
+    padding-left: 9px;
+    color: #8FC549;
+
+`
+const MyTodayHabits = styled.div`
+    margin: 28px auto;
+`
 
 const MyHabitBox = styled.div`
     width: 340px;
-    height: 100%;    
-    background: lightblue;
+    min-height: 94px;
+    background: #FFFFFF;
     border-radius: 5px;
     display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
 
+
+    h1{
+        padding: 13px 100px 7px 15px;
+        font-size: 20px;
+        line-height: 25px;
+        color: #666666;
+    }
 
     button{
         box-sizing: border-box;
         width: 69px;
         height: 69px;
+        margin: auto 15px;
             
         background-color: ${props => props.checked ? '#8FC549' : '#EBEBEB' };
         border: 1px solid #E7E7E7;
         border-radius: 5px;
     }
 
+    p{
+        font-size: 13px;
+        line-height: 16px;
+        color: #666666;
+    }
+
     h5{
+        font-size: 13px;
+        line-height: 16px;
         color: ${props => props.checked ? '#8FC549' : '#666666' };
     }
 
     h6{
+        font-size: 13px;
+        line-height: 16px;
         color: ${props => props.isRecord ? '#8FC549' : '#666666' };
     }
 
+`
 
+const Text = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 15px;
 `
